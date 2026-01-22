3cc0589068c0df6b1bfb687b2d4cc834cad18fbc
jeronimo <jeropuntoyo@gmail.com>
2026-01-22 16:11:00 -0300

    UI: autoscroll to posts/listings and login errors animations

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -65,6 +65,7 @@ export class AuthController {
                             <input name="email" type="email" placeholder="Email" required autocomplete="email"/>
                             <input name="password" type="password" placeholder="Contraseña" required autocomplete="new-password"/>
                             <input name="name" placeholder="Usuario" required autocomplete="username"/>
+                            <br>
                             <button type="submit" style="margin-top: 1rem;margin-bottom: 0.5rem;"> Registrarse </button>
                         </form>
 
@@ -136,13 +137,13 @@ export class AuthController {
 
                             <input name="email" type="email" placeholder="Email" required />
                             <input name="password" type="password" placeholder="Contraseña" required />
-                            <button type="submit" class="log-btn"> Iniciar sesión </button>
+                            <br>
+                            <button type="submit" class="log-btn" style="margin-top: 1rem;margin-bottom: 0.5rem;"> Iniciar sesión </button>
                         </form>
 
                         <div id="login-error"></div>
-                        <p>
-                            ¿No tenés cuenta? <a class="log-btn" href="/auth/register"> Registrarse </a>
-                        </p>
+                        <p>¿No tenés cuenta?</p>
+                        <a class="log-btn" href="/auth/register"> Registrarse </a>
                     </div>
                 </main>
                 <script> loadHeader("Iniciar sesión"); loadFooter();</script>
